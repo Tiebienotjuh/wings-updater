dbe3d05144d2dadf6acee01a81c38147974908f8
Tiebienotjuh <74455765+Tiebienotjuh@users.noreply.github.com>
2023-02-09 19:55:23 +0100

    Update wings-updater.sh

--- wings-updater.sh
+++ wings-updater.sh
@@ -1,26 +1,32 @@
-#Copyright (c) 2023 Tiebienotjuh
-#
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
 
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
+#!/bin/bash
+set -e
+
+####################################################################################
+# Copyright (c) 2023 Tiebienotjuh                                                  #
+#                                                                                  #
+# Permission is hereby granted, free of charge, to any person obtaining a copy     #
+# of this software and associated documentation files (the "Software"), to deal    #
+# in the Software without restriction, including without limitation the rights     #
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        #
+# copies of the Software, and to permit persons to whom the Software is            #
+# furnished to do so, subject to the following conditions:                         #
+#                                                                                  #
+# The above copyright notice and this permission notice shall be included in all   #
+# copies or substantial portions of the Software.                                  #
+#                                                                                  #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    #
+# SOFTWARE.                                                                        #
+####################################################################################
 
 
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/pterodactyl/wings/releases/latest" | jq -r .tag_name
+    curl --silent "https://api.github.com/repos/pterodactyl/wings/releases/latest" | jq -r .tag_name
 }
 
 update() {
@@ -34,16 +40,30 @@ update() {
 
 if [[ $1 == "check" ]]; then
     echo "Checking for updates..."
-    latest_release=$(get_latest_release)
+    current_release=$(sudo wings version | awk '{print $2}' | awk '{ print substr( $0, 2) }')
+    latest_release=$(get_latest_release | awk '{ print substr( $0, 2) }')
     if [ $latest_release != $current_release ]; then
         echo "Update available!"
         echo "Current version: $current_release"
         echo "Latest version: $latest_release"
         # call update function
         update
-        
+    elif [ $latest_release = $current_release ]; then
+        echo "No update available"
+        echo "Current version: $current_release"
+        echo "Latest version: $latest_release"
+    else
+        echo "Something went wrong"
     fi
+elif [[ $1 == "update" ]]; then
+    update
 else
+    # Check if wings are installed and configured
+    if ! [ -x "$(command -v wings)" ]; then
+        echo "Wings is not installed!"
+        echo "Please install wings first"
+        exit
+    fi
     # Check if script is installed
     if [ -f /usr/local/bin/wings-updater.sh ]; then
         echo "Script is already installed!"
